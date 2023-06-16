de88eba0055efc2f8f823dc66a4c1efd8543e493
Agustin Brambilla <agustinbram@gmail.com>
2023-06-16 14:36:08 -0300

    Class Player Finished

--- torneos/src/net/app/entities/Player.java
+++ torneos/src/net/app/entities/Player.java
@@ -3,9 +3,43 @@ package net.app.entities;
 import net.app.interfaces.IPlayer;
 
 public class Player implements IPlayer {
-    public Player(String name) {
-        // TODO: initialize Player data
+    String name;
+    Integer totalScore;
+
+    public Player(String name, Integer totalScore) {
+        //Done: initialize Player data
+        this.name = name;
+        this.totalScore = totalScore;
+        
     }
-    // TODO: implement the propper interface
+
+    // Done: implement the propper interface
+    @Override
+    public String getName() {
+        //Done Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getName'");
+        return name;
+    }
+
+    @Override
+    public int getTotalScore() {
+        //Done: Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getTotalScore'");
+        return totalScore;
+    }
+
+    @Override
+    public void incrementTotalScore() {
+        //Done: Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'incrementTotalScore'");
+        totalScore += 1;
+    }
+
+    @Override
+    public void decrementTotalScore() {
+        //Done: Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'decrementTotalScore'");
+        totalScore -= 1;
+    }
 
 }
